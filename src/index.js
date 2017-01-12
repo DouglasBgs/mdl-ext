3896bd8a4d819d5506c956f90242054c13c32a86
Leif Olsen <leif.o.olsen@gmail.com>
2017-01-12 12:21:03 +0100

    feat(build): multipart build

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 /* istanbul ignore next */
 import './utils';
+
 /* istanbul ignore next */
 import './sticky-header/sticky-header';
+
 /* istanbul ignore next */
 import './selectfield/selectfield';
+
 /* istanbul ignore next */
 import './accordion/accordion';
+
 /* istanbul ignore next */
 import './lightboard/lightboard';
+
 /* istanbul ignore next */
 import './lightbox/lightbox';
+
 /* istanbul ignore next */
 import './carousel/carousel';
 
-//import './menu-button/menu-button';
 /* istanbul ignore next */
-export { default as MaterialExtMenuButton } from './menu-button/menu-button';
+import './menu-button/menu-button';
+//export { default as MaterialExtMenuButton } from './menu-button/menu-button';
---
+++ webpack.config.multipart.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.mode;
+
+const libraryName = 'mdl-ext';
+var outputFile;
+
+if (env === 'build') {
+  outputFile = libraryName + '.[name].min.js';
+}
+else {
+  outputFile = libraryName + '.[name].js';
+}
+
+var config = {
+  entry: {
+    'common': path.join(__dirname, 'src/utils/index.js'),
+    'accordion': path.join(__dirname, 'src/accordion/accordion.js'),
+    'carousel': path.join(__dirname, 'src/carousel/carousel.js'),
+    'lightboard': path.join(__dirname, 'src/lightboard/lightboard.js'),
+    'lightbox': path.join(__dirname, 'src/lightbox/lightbox.js'),
+    'menu-button': path.join(__dirname, 'src/menu-button/menu-button.js'),
+    'selectfield': path.join(__dirname, 'src/selectfield/selectfield.js'),
+    'sticky-header': path.join(__dirname, 'src/sticky-header/sticky-header.js'),
+  },
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: outputFile,
+    library: [libraryName, '[name]'],
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    modulesDirectories: ['src', 'node_modules'],
+    extensions: ['', '.js', '.jsx', '.css', '.scss', 'html']
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ],
+};
+
+module.exports = config;
